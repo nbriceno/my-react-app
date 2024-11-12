86aa0194cdffc0a9a3c56c41555c01a26cbe0731
nbriceno <briceno@wustl.edu>
2024-11-12 16:52:50 -0600

    clicked interactive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 function Square({ value }) {
-  return <button className="square">{value}</button>;
-}
+  function handleClick() {
+    console.log('clicked!');
+  }
 
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
 
 export default function Board() {
   return (
