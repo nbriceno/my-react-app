5234ff56c87517b316e4e672fc92e0189a0ad9cf
nbriceno <briceno@wustl.edu>
2024-11-12 18:36:22 -0600

    Lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function Square({value, onSquareClick}) {
   );
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -53,6 +53,18 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
